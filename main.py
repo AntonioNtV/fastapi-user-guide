6705fa20f2b67fa09ec0ddffcc559a3446bc0f70
Antonio Bertino <antonio.bertin.neto@ccc.ufcg.edu.br>
2021-10-19 09:16:56 -0300

    Feature(main): Using enum as path variable

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
@@ -13,3 +19,13 @@ async def get_cachorro():
 async def get_item_by_id(item_id: int):
     if item_id - 1 >= len(items): raise Exception("Invalid Item Id")
     return {"item": items[item_id - 1]}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
