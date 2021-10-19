87af4602a499965c245324808db801bf7cabe4ba
Antonio Bertino <antonio.bertin.neto@ccc.ufcg.edu.br>
2021-10-19 09:12:38 -0300

    Feature(main): Order of routing matter. No parameter endpoints comes first

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ app = FastAPI()
 
 items = ["Cachorro", "Gato", "Rato", "Unicornio"]
 
+
+@app.get("/items/cachorro")
+async def get_cachorro():
+    return {"item": items[0]}
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
+async def get_item_by_id(item_id: int):
     if item_id - 1 >= len(items): raise Exception("Invalid Item Id")
-    return {"item": items[item_id]}
+    return {"item": items[item_id - 1]}
