aeaa1cbb1588de285a49e94a3fc14d826428330c
Antonio Bertino <antonio.bertin.neto@ccc.ufcg.edu.br>
2021-10-19 09:07:20 -0300

    Refactor(main): Type of path variable

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 items = ["Cachorro", "Gato", "Rato", "Unicornio"]
 
 @app.get("/items/{item_id}")
-async def read_item(item_id):
-    if int(item_id) - 1 >= len(items): raise Exception("Invalid Item Id")
-    return {"item": items[int(item_id)]}
+async def read_item(item_id: int):
+    if item_id - 1 >= len(items): raise Exception("Invalid Item Id")
+    return {"item": items[item_id]}
